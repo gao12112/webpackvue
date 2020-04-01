a2f9b37695dd34fdb97428426515f05bf442c97f
gao12112 <1157810112@qq.com>
2020-04-01 11:45:40 +0800

    vuex 状态刷新仍存在vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import persistedState from "vuex-persistedstate"
+import * as Cookies from "js-cookie"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // plugins: [persistedState()],
+  // plugins: [persistedState({ storage: window.sessionStorage })],
+  plugins: [
+    persistedState({
+      storage: {
+        getItem: key => Cookies.get(key),
+        setItem: (key, value) => Cookies.set(key, value, { expires: 7 }),
+        removeItem: key => Cookies.remove(key)
+      }
+    })
+  ],
   state: {
-    shopid: 1
+    shopid: 1,
+    token: ""
   },
   mutations: {},
   actions: {},
